87b4087a98fdbb9a3d616b6cbf249dc99e7595d1
Ubuntu <ubuntu@ip-172-31-14-176.us-west-2.compute.internal>
2013-07-14 05:14:29 +0000

    Attempted bug fix again

--- web.js
+++ web.js
@@ -6,10 +6,10 @@ var buf = new Buffer(fs.readFileSync('index.html'));
 var text = buf.toString('utf8');
 
 app.get('/', function(request, response) {
-  response.send("Derp");
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-   console.log("Listening on " + port);
+  console.log("Listening on " + port);
 });
